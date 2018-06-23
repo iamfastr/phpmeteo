8585e089f40ac026e537dbaa85b68dc353338d61
iamfastr <40510034+iamfastr@users.noreply.github.com>
2018-06-23 05:58:13 +0300

    Create russian.php
    Русский язык для phpMeteo

---
+++ russian.php
@@ -0,0 +1,234 @@
+<?php
+// Language Name
+$name = 'Русский';
+
+// Language Author
+$author = 'IAMFASTR';
+
+// Language URL
+$url = 'https://github.com/iamfastr/phpmeteo/';
+
+// Language Direction
+$direction = 'ltr';
+
+$lang['info'] = 'Информация';
+$lang['themes'] = 'Темы';
+$lang['language'] = 'Язык';
+$lang['languages'] = 'Языки';
+
+// Admin Panel
+$lang['admin'] = 'Админка';
+$lang['general'] = 'Основные настройки';
+$lang['password'] = 'Пароль';
+$lang['info_pages'] = 'Информацонные страницы';
+$lang['username'] = 'Имя пользователя';
+$lang['password'] = 'Пароль';
+$lang['login'] = 'Войти';
+$lang['logout'] = 'Выйти';
+$lang['save'] = 'Сохранить';
+$lang['remember_me'] = 'Запомнить меня';
+$lang['recover'] = 'Восстановить';
+$lang['by'] = 'От';
+$lang['active'] = 'Активно';
+$lang['activate'] = 'Активировать';
+$lang['default'] = 'По умолчанию';
+$lang['make_default'] = 'Сделать по умолчанию';
+$lang['public'] = 'Публично';
+$lang['unlisted'] = 'Не в списк';
+$lang['edit'] = 'Редактировать';
+$lang['new_page'] = 'Новая страница';
+$lang['delete'] = 'Удалить';
+$lang['cancel'] = 'Отмена';
+$lang['yes'] = 'Да';
+$lang['no'] = 'Нет';
+$lang['off'] = 'Откл.';
+$lang['on'] = 'Вкл.';
+
+$lang['dashboard'] = 'Панель управления';
+$lang['site_title'] = 'Название сайта';
+$lang['site_logo'] = 'Логотип сайта';
+$lang['timezone'] = 'Временная зона';
+$lang['tracking_code'] = 'Код отслеживания';
+$lang['current_password'] = 'Текущий пароль';
+$lang['new_password'] = 'Новый пароль';
+$lang['confirm_new_password'] = 'Подтвердите пароль';
+
+$lang['settings_saved'] = 'Изменения сохранены';
+
+$lang['upload_error_code'] = 'Ошибка загрузки файла. Код ошибки: %s';
+$lang['invalid_user_pass'] = 'Неверно указан логин и/или пароль';
+$lang['wrong_current_password'] = 'Неверно указан пароль';
+$lang['password_too_short'] = 'Вы ввели слишком короткий пароль';
+$lang['password_not_matching'] = 'Введенные пароли не совпадают';
+$lang['all_fields_required'] = 'Необходимо заполнить все поля';
+
+$lang['page_title'] = 'Название страницы';
+$lang['page_url'] = 'URL страницы';
+$lang['page_public'] = 'Видимость страницы';
+$lang['page_content'] = 'Контент';
+$lang['page_url_exists'] = 'Страница с таким URL существует';
+$lang['page_created'] = 'Страница %s создана';
+$lang['page_deleted'] = 'Страница %s удалена';
+
+$lang['site_info'] = 'Информация о сайте';
+$lang['useful_links'] = 'Полезные ссылки';
+$lang['get_more_themes'] = 'Найти больше тем';
+$lang['get_more_languages'] = 'Найти больше языков';
+
+$lang['ads'] = 'Реклама';
+$lang['ads_1'] = 'Главная страница (Верх)';
+$lang['ads_2'] = 'Главная страница (Центр)';
+$lang['ads_3'] = 'Главная страница (Низ)';
+$lang['ad_unit_code'] = 'Код рекламы';
+
+$lang['weather_api_key'] = 'Ключ API';
+$lang['weather_location'] = 'Местоположение по умолчанию';
+$lang['weather_format'] = 'Единица измерения температуры по умолчанию';
+$lang['weather_geo_location'] = 'Геолокация';
+$lang['weather_forecast_days'] = 'Прогнозируемые дни (от 1 до 5)';
+$lang['weather_latest'] = 'Последние поиское запросы';
+
+// Home
+$lang['preferences'] = 'НАСТРОЙКИ';
+
+// Preferences
+$lang['theme'] = 'Тема';
+$lang['dark_mode'] = 'Включить темную тему';
+$lang['site_language'] = 'Язык сайта';
+
+// Misc
+$lang['token_mismatch'] = 'Несоответствие токена';
+$lang['copyright'] = 'Copyright &copy; %s %s.';
+$lang['all_rights_reserved'] = 'All rights reserved.';
+$lang['powered_by'] = 'Proudly Powered by %s.';
+
+// Error
+$lang['no_results_found'] = 'Нет результатов для %s. Повторите попытку позже.';
+$lang['suggestions'] = 'Решения:';
+$lang['suggestion_1'] = 'Попробуйте поискать по названию города и по коду страны (RU для России).';
+$lang['suggestion_2'] = 'Попробуйте поискать ближайшие города.';
+$lang['suggestion_3'] = 'Повторите попытку поиска через какое-то время.';
+$lang['suggestion_4'] = 'Напишите email администратору о проблеме (support@onyx.ltd).';
+
+// Weather
+$lang['weather'] = 'Погода';
+$lang['forecast'] = 'Прогноз';
+$lang['evolution'] = 'Изменение';
+$lang['conditions'] = 'Условия';
+$lang['lowest_temperature'] = 'Самая низкая температура';
+$lang['highest_temperature'] = 'Самая высокая температура';
+$lang['temperature'] = 'Температура';
+$lang['humidity'] = 'Влажность';
+$lang['wind_speed'] = 'Скорость ветра';
+$lang['wind_direction'] = 'Направление ветра';
+$lang['city_name'] = 'Что будем искать?';
+$lang['latest_searches'] = 'Последние поисковые запросы';
+$lang['date_format'] = '%2$s/%1$s/%3$s';
+$lang['hour_format'] = '%1$s:%2$s';
+$lang['kmph'] = 'км/ч';
+$lang['mph'] = 'мили/ч';
+$lang['monday'] = 'Понедельник';
+$lang['tuesday'] = 'Вторник';
+$lang['wednesday'] = 'Среда';
+$lang['thursday'] = 'Четверг';
+$lang['friday'] = 'Пятница';
+$lang['saturday'] = 'Суббота';
+$lang['sunday'] = 'Воскресенье';
+$lang['favorites'] = 'Избранное';
+$lang['search_results'] = 'Поисковые запросы';
+$lang['daily_forecast_evolution'] = 'Ежедневный прогноз изменений погоды';
+$lang['hourly_forecast_evolution'] = 'Почасовой прогноз изменений погоды';
+$lang['daily_forecast'] = 'Ежедневный прогноз';
+$lang['hourly_forecast'] = 'Почасовой прогноз';
+$lang['sunrise'] = 'Рассвет';
+$lang['sunset'] = 'Закат';
+$lang['latitude'] = 'Широта';
+$lang['longitude'] = 'Долгота';
+$lang['timezone_gmt'] = 'Часовая зона: GMT%s';
+
+$lang['f'] = 'F';
+$lang['c'] = 'C';
+$lang['fahrenheit'] = 'Фаренгейт';
+$lang['celsius'] = 'Цельсия';
+
+$lang['c_200'] = 'Гроза с небольшим дождем';
+$lang['c_201'] = 'Гроза с дождем';
+$lang['c_202'] = 'Гроза с сильным дождем';
+$lang['c_210'] = 'Глабая гроза';
+$lang['c_211'] = 'Гроза';
+$lang['c_212'] = 'Сильная гроза';
+$lang['c_221'] = 'Местами гроза';
+$lang['c_230'] = 'Гроза, слабая изморось';
+$lang['c_231'] = 'Гроза, изморось';
+$lang['c_232'] = 'Гроза, сильная изморось';
+
+$lang['c_300'] = 'Слабая изморось';
+$lang['c_301'] = 'изморось';
+$lang['c_302'] = 'Сильная изморось';
+$lang['c_310'] = 'Слабый дождь, изморось';
+$lang['c_311'] = 'Дождь, изморось';
+$lang['c_312'] = 'Сильный дождь, изизморось';
+$lang['c_313'] = 'Ливневый дождь, изизморось';
+$lang['c_314'] = 'Сильный ливневый дождь, изморось';
+$lang['c_321'] = 'Дождь с изморосью';
+
+$lang['c_500'] = 'Легкий дождь';
+$lang['c_501'] = 'Средний дождь';
+$lang['c_502'] = 'Сильный дождь';
+$lang['c_503'] = 'Очень сильный дождь';
+$lang['c_504'] = 'Сильный дождь';
+$lang['c_511'] = 'Ледяной дождь';
+$lang['c_520'] = 'Легкий ливневый дождь';
+$lang['c_521'] = 'Ливневый дождь';
+$lang['c_522'] = 'Сильный ливневый дождь';
+$lang['c_531'] = 'Местами ливневый дождь';
+
+$lang['c_600'] = 'Легкий снегопад';
+$lang['c_601'] = 'Снегопад';
+$lang['c_602'] = 'Сильный снегопад';
+$lang['c_611'] = 'Дождь со снегом';
+$lang['c_612'] = 'Мокрый снег';
+$lang['c_615'] = 'Легкий дождь и снег';
+$lang['c_616'] = 'Дождь и снег';
+$lang['c_620'] = 'Легкий мокрый снег';
+$lang['c_621'] = 'Ливневый снег';
+$lang['c_622'] = 'Сильный ливневый снег';
+
+$lang['c_701'] = 'Туман';
+$lang['c_711'] = 'Дымка';
+$lang['c_721'] = 'Сухой туман';
+$lang['c_731'] = 'Песок, пылевые завихрения';
+$lang['c_741'] = 'Туман';
+$lang['c_751'] = 'Песок';
+$lang['c_761'] = 'Пыль';
+$lang['c_762'] = 'Вулканический пепел';
+$lang['c_771'] = 'Шквал';
+$lang['c_781'] = 'Ураган';
+
+$lang['c_800'] = 'Чистое небо';
+
+$lang['c_801'] = 'Малооблачно';
+$lang['c_802'] = 'Рассеянные облака';
+$lang['c_803'] = 'Облачно';
+$lang['c_804'] = 'Сильная облачность';
+
+$lang['c_900'] = 'Торнадо';
+$lang['c_901'] = 'Тропическая буря';
+$lang['c_902'] = 'Ураган';
+$lang['c_903'] = 'Холодно';
+$lang['c_904'] = 'Жарко';
+$lang['c_905'] = 'Ветренно';
+$lang['c_906'] = 'Град';
+
+$lang['c_951'] = 'Затишье';
+$lang['c_952'] = 'Легкий ветер';
+$lang['c_953'] = 'Нежный ветер';
+$lang['c_954'] = 'Умеренный ветер';
+$lang['c_955'] = 'Свежий ветер';
+$lang['c_956'] = 'Сильный ветер';
+$lang['c_957'] = 'Сильный ветер, почти буря';
+$lang['c_958'] = 'Буря';
+$lang['c_959'] = 'Сильная буря';
+$lang['c_960'] = 'Шторм';
+$lang['c_961'] = 'Сильный шторм';
+$lang['c_962'] = 'Ураган/Смерчь';
